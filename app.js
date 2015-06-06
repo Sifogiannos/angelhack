888d945989ece30ce981168d95cfb00030985da3
Giannis Sofianos <sifogiannos@gmail.com>
2015-06-06 12:43:23 +0300

    authendication routes

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
+//require database file
+require('./db.js');
+var mongoose = require('mongoose');
+
+//authentication requirements
+var passport = require('passport');
+var flash = require('connect-flash');
+var LocalStrategy = require('passport-local').Strategy;
+
+
 var express = require('express');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+//passport file
+require('./pass.js')(passport, LocalStrategy);
+
+var authentication = require('./routes/authentication.js');
+
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var user = require('./routes/users');
 
 var app = express();
 
@@ -20,10 +37,36 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret: "angelhack",
+  store: new MongoStore({
+    db: 'sessionstore',
+    mongooseConnection: mongoose.connections[0]
+  })
+}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/users', user);
+
+//authentication
+app.get('/login', authentication.login);
+app.get('/signup', authentication.signup);
+app.post('/create_user', authentication.createUser);
+app.post('/login',
+  passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }),
+  function(req, res){
+    res.redirect('/');
+    //return res.json({status:"ok", data:req.user});
+  }
+);
+
+app.get('/logout', authentication.logout);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/authentication.js
@@ -0,0 +1,77 @@
+var passport = require('passport');
+var flash = require('connect-flash');
+var LocalStrategy = require('passport-local').Strategy;
+require('../pass.js')(passport, LocalStrategy);
+var crypto    = require('crypto');
+var key       = 'secret';
+var algorithm = 'sha1';
+var hash, hmac;
+var mongoose = require( 'mongoose' );
+var users = mongoose.model( 'users', users );
+
+
+exports.login = function(req, res){
+  if(req.user)
+    return res.redirect('/');
+  //if not logged in sign in
+  res.render('sign_in',{
+    message: req.flash('error')
+  });
+};
+
+exports.signup = function(req, res){
+  if(req.user)
+    return res.redirect('/');
+  return res.render('sign_up');
+};
+
+exports.logout = function(req, res){
+  if(req.user){
+    req.logout();
+    //after logout what to do
+    return res.redirect('/login');
+  }
+  return res.redirect({status:"error", message:"you are not logged in to logout"});
+}
+
+exports.createUser = function(req, res){
+  
+  //create password hash
+  hash = generateHash(req.body.password);
+
+  var mail = req.body.username.toLowerCase();
+  var user = new users({
+    company       : req.body.company,
+    email         : mail,
+    password      : hash
+  });
+  users.findOne({email:mail}).exec(function(err, user_exists){
+    if(err) 
+    	return res.json({status:"error", message:"error occured"});
+    if(user_exists)
+      return res.json({status:"error", message:"the user already exists"});
+    if(!user_exists){
+      user.save( function(err, user){
+        if(err)
+        	return res.json({status:"error", message:"could not save new user"});
+        req.logIn(user, function(err) {
+          if (err) 
+          	return res.json({status:"error", message:"error occured"});
+
+      		return res.redirect('/');
+        });
+      });
+    }
+  });
+};
+
+function generateHash(text){
+  hmac = crypto.createHmac(algorithm, key);
+  // change to 'binary' if you want a binary digest
+  hmac.setEncoding('hex');
+  // write in the text that you want the hmac digest for
+  hmac.write(text);
+  // you cannot read from the stream until you call end()
+  hmac.end();
+  return  hmac.read();
+}
