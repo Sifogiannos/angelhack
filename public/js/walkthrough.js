5b996f6d95d5dd4ed3a9f031d9d22209b323ff5b
Nikos Rossolatos <spidey_nr@hotmail.com>
2015-06-06 17:32:58 +0300

    initiating js walkthrough

--- public/js/walkthrough.js
+++ public/js/walkthrough.js
@@ -1,16 +1,63 @@
 (function(){
 	var submitButton = document.getElementById('submitButton');
 	var stepForms = document.querySelectorAll('.steps form');
+	var insigniaInput = document.getElementById('insignia');
+	var ajax = ajax();
+	insignia(insigniaInput);
 	var step = 0;
 	initListeners();
 
 
 	function initListeners(){
-
-		crossvent.add(submitButton,'click',nextStep);
+		crossvent.add(submitButton,'click',postData);
 	}
-	function nextStep(e){
+	function postData(e){
 		e.preventDefault();
+		if(step==0){
+			var data = {
+				name:document.getElementById('fname').value,
+				surname:document.getElementById('lname').value,
+				jobTitle:document.getElementById('jobTitle').value,
+				company:document.getElementById('company').value
+			}
+			console.log(data);
+			// $.ajax({
+			// 	url: '/path/to/file',
+			// 	type: 'PUT',
+			// 	data: {param1: 'value1'},
+			// })
+			// .done(function() {
+			// 	console.log("success");
+			// });
+		}
+		else if(step==1){
+			// $.ajax({
+			// 	url: '/path/to/file',
+			// 	type: 'PUT',
+			// 	data: {param1: 'value1'},
+			// })
+			// .done(function() {
+			// 	console.log("success");
+			// });
+		}
+		else if(step==2){
+			// $.ajax({
+			// 	url: '/path/to/file',
+			// 	type: 'PUT',
+			// 	data: {param1: 'value1'},
+			// })
+			// .done(function() {
+			// 	console.log("success");
+			// });
+		}
+		else{
+			//do nothing. Propably error something
+			alert("something is wrong, try refreshing")
+		}
+		nextStep();
+	}
+	function nextStep(){
+		
 		//post data here
 		if(step==2){
 
@@ -25,5 +72,54 @@
 			step++;
 			stepForms[step].parentNode.className = 'step show'
 		}
+	}
+	function ajax() {
+		var xmlhttp;
+		function generateXhr(callback){
+			// code for IE7+, Firefox, Chrome, Opera, Safari
+	        xmlhttp = new XMLHttpRequest();
+
+		    xmlhttp.onreadystatechange = function() {
+		        if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
+		           if(xmlhttp.status == 200){
+		              //success
+		              callback();
+		           }
+		           else if(xmlhttp.status == 400) {
+		              //error
+		           }
+		           else {
+		               //biggest error
+		           }
+		        }
+		    }
+		    return xmlhttp;
+		}
+	    function get(url,data,callback){
+			var xhr = generateXhr(callback);
+			xmlhttp.open("GET", url, true);
+	   		xmlhttp.send(data);
+	    }
+	    function post(url,data){
+			var xhr = generateXhr(callback);
+			xmlhttp.open("GET", url, true);
+	   		xmlhttp.send(data);
+	    }
+	    function put(url,data){
+			var xhr = generateXhr(callback);
+			xmlhttp.open("GET", url, true);
+	   		xmlhttp.send(data);
+	    }
+	    function del(url,data){
+			var xhr = generateXhr(callback);
+			xmlhttp.open("GET", url, true);
+	   		xmlhttp.send(data);
+	    }
+	    return {
+	    	get:get,
+	    	post:post,
+	    	put:put,
+	    	del:del
+	    }
 	}
 })();
