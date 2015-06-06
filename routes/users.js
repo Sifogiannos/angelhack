372952bbfc88bee1421291a5869febfd28459723
Giannis Sofianos <sifogiannos@gmail.com>
2015-06-06 20:38:18 +0300

    find similar user of the event

--- routes/users.js
+++ routes/users.js
@@ -3,14 +3,15 @@ var router = express.Router();
 
 var mongoose = require( 'mongoose' );
 var users = mongoose.model( 'users', users );
+var events = mongoose.model( 'events', events );
 
 
-router.get('/', function(req, res) {
+router.get('/events/:event_id', function(req, res) {
   if(!req.user){
 		return res.json({status:"error", message:"you are not logged in"});
 	}
-	users.find({}, function(err, allUsers){
-		return res.json({status:"ok", allUsers:allUsers});
+	events.findOne({_id:event_id}).populate('participants').exec(function(err, event){
+		return res.json({status:"ok", allUsers:event.participants});
 	});
 });
 
